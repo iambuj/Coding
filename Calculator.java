a8a3783b98d6514278863f42e52930a549a05db1
iambuj <abhinavtripathi20008@gmail.com>
2026-08-24 00:08:43 +0530

    add Calculator program

---
+++ Calculator.java
@@ -0,0 +1,23 @@
+
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+
+        int sum = a+b;
+        int diff = a-b;
+        int prod = a*b;
+        int div = a/b;
+
+
+        System.out.print("Sum is = ");
+        System.out.println(sum);
+        System.out.print("diff is = ");
+        System.out.println(diff);
+        System.out.print("Product is = ");
+        System.out.println(prod);
+        System.out.print("Division is = ");
+        System.out.println(div);
+    }
+}
